08153a4a32c90eead5b8000f551c4b580c211997
Hillary <hmupfumi@gmail.com>
2026-08-18 22:05:36 -0400

    MCP servers

---
+++ mcp-servers/main.py
@@ -0,0 +1,11 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    print("Hello from mcp-servers!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
